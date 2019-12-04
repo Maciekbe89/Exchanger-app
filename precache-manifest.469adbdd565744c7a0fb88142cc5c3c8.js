8710fe4ff91f67da8dec4b961604f74bb4274c94
MaciekBe89 <maciejb@virtualdesign.pl>
2019-12-04 18:20:41 +0100

    Updates

---
+++ precache-manifest.469adbdd565744c7a0fb88142cc5c3c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01048aec47cb1770815122f86ec29f0",
+    "url": "/Exchanger-app/index.html"
+  },
+  {
+    "revision": "365229afd23c8bf4313b",
+    "url": "/Exchanger-app/static/css/main.4f10153c.chunk.css"
+  },
+  {
+    "revision": "876415c5288098d9768b",
+    "url": "/Exchanger-app/static/js/2.d98f0b3e.chunk.js"
+  },
+  {
+    "revision": "365229afd23c8bf4313b",
+    "url": "/Exchanger-app/static/js/main.a9dcf39c.chunk.js"
+  },
+  {
+    "revision": "2c57b8f9aa7544c6920c",
+    "url": "/Exchanger-app/static/js/runtime-main.285ac5d2.js"
+  },
+  {
+    "revision": "075aba9c70bc2c2f7c885a7c7d914fe5",
+    "url": "/Exchanger-app/static/media/eu.075aba9c.png"
+  },
+  {
+    "revision": "d3ddd6025a06a78535b0d432d14905bf",
+    "url": "/Exchanger-app/static/media/gb.d3ddd602.svg"
+  },
+  {
+    "revision": "562edca5bb39d66f4c9238a36295187b",
+    "url": "/Exchanger-app/static/media/pl.562edca5.svg"
+  },
+  {
+    "revision": "05615112706e0396ff8c91eb9b6c05f2",
+    "url": "/Exchanger-app/static/media/usa.05615112.svg"
+  }
+]);
